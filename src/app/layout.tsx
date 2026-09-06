eaf73a71200c3b195a3aaf21c2ed33c602c0bcbf
Henry Goodluck <chigoziehenry852@gmail.com>
2026-09-06 22:47:06 +0100

    update UI files

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -23,7 +23,11 @@ interface LayoutProps {
   children: React.ReactNode;
 }
 
-export default function RootLayout({ children }: LayoutProps<"/">) {
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
   return (
     <html
       lang="en"
